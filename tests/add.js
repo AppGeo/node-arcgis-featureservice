86da676ee584192ac5f14e258785dd2f50c14726
Ishmael Smyrnow <ismyrnow@appgeo.com>
2015-01-01 11:27:03 -0500

    Test error handling in add(), fix for nock
    - Need to call nock.cleanAll() to reset the filters from other tests.
    - Test to check for proper error handling when calling add() (will be similar for other methods).

--- tests/add.js
+++ tests/add.js
@@ -1,12 +1,39 @@
 var test = require('tape');
+var nock = require('nock');
 var FeatureService = require('../src/index');
 
+var svc = new FeatureService({
+  url: 'http://fake.map.service',
+  idField: 'id'
+});
+
 test('has add method', function (t) {
-  var svc = new FeatureService({
-    url: 'http://fake.map.service',
-    idField: 'id'
-  });
-  
   t.equals(typeof svc.add, 'function', '`add` is a function');
   t.end();
+});
+
+test('add method wraps server error', function (t) {
+  nock.cleanAll();
+  nock('http://fake.map.service')
+    .filteringPath(function (path) {
+      return '/addFeatures'
+    })
+    .post('/addFeatures')
+    .reply(200, {
+      addResults: [
+        {
+          "success": false,
+          "error": {
+            "code": -2147217395,
+            "description": "Setting of Value for depth failed."
+          }
+        }
+      ]
+    });
+  
+  svc.add({ foo: 'bar' }, function (err, data) {
+    t.ok(err instanceof Error, '`err` is an instance of Error');
+    t.equals(err.message, 'Setting of Value for depth failed.', '`err.message` is as expected');
+    t.end();
+  });
 });
--- tests/get.js
+++ tests/get.js
@@ -2,39 +2,36 @@ var test = require('tape');
 var nock = require('nock');
 var FeatureService = require('../src/index');
 
+var svc = new FeatureService({
+  url: 'http://fake.map.service',
+  idField: 'id'
+});
+
 test('has get method', function (t) {
-  var svc = new FeatureService({
-    url: 'http://fake.map.service',
-    idField: 'id'
-  });
-  
   t.equals(typeof svc.get, 'function', '`get` is a function');
   t.end();
 });
 
 test('get method makes the correct api call', function (t) {
-  var svc = new FeatureService({
-    url: 'http://fake.map.service',
-    idField: 'id'
-  });
-  
+  nock.cleanAll();
   nock('http://fake.map.service')
-  .filteringPath(function (path) {
-    return '/query';
-  })
-  .get('/query')
-  .reply(200, {
-    junk: true,
-    features: [
-    {
-      attributes: {
-        foo: 'bar'
+    .filteringPath(function (path) {
+      return '/query';
+    })
+    .get('/query')
+    .reply(200, {
+      junk: true,
+      features: [
+      {
+        attributes: {
+          foo: 'bar'
+        }
       }
-    }
-    ]
-  });
+      ]
+    });
   
   svc.get({ foo: 'bar' }, function (err, data) {
+    t.notOk(err, '`err` is not defined');
     t.equals(data.type, 'FeatureCollection', 'result is a FeatureCollection');
     t.equals(data.features.length, 1, 'result has features');
     t.equals(data.features[0].properties.foo, 'bar', 'result\'s features have the right properties');
@@ -43,21 +40,17 @@ test('get method makes the correct api call', function (t) {
 });
 
 test('get method wraps server error', function (t) {
-  var svc = new FeatureService({
-    url: 'http://fake.map.service',
-    idField: 'id'
-  });
-  
+  nock.cleanAll();
   nock('http://fake.map.service')
-  .filteringPath(function (path) {
-    return '/query';
-  })
-  .get('/query')
-  .reply(200, {
-    error: {
-      message: 'fake error message'
-    }
-  });
+    .filteringPath(function (path) {
+      return '/query';
+    })
+    .get('/query')
+    .reply(200, {
+      error: {
+        message: 'fake error message'
+      }
+    });
   
   svc.get({ foo: 'bar' }, function (err, data) {
     t.ok(err instanceof Error, '`err` is an instance of Error');
